78e31dab5d8c061fe2e7bcca0b6816b65cd1ed46
aakash ranglani <ranglaniaakash.git@gmail.com>
2021-11-30 22:32:16 +0000

    extracting individual csv files

--- fun.py
+++ fun.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-crop = 'Banana'
+crop = 'Rice'
 
 dataset = pd.DataFrame(columns=['State', 'Year', 'Produce', 'Temperature', 'Rainfall'])
 
@@ -12,26 +12,35 @@ rainfall_df = pd.read_csv('rainfall.csv')
 state_list = sorted(df['State_Name'].unique())
 year_list = sorted(df['Crop_Year'].unique())
 
-print(state_list)
+df['State_Name'] = df['State_Name'].str.strip()
 
-temperature_df = temperature_df[temperature_df.columns.drop(list(temperature_df.filter(regex='Administrative unit not ava*')))]
-print(sorted(temperature_df.columns.unique()))
+# print(state_list)
+# print(sorted(rainfall_df["SUBDIVISION"].unique()))
+
+# temperature_df = temperature_df[
+#     temperature_df.columns.drop(list(temperature_df.filter(regex='Administrative unit not ava*')))]
 
 for state in state_list:
     # remove "Andaman and Nicobar Islands" and replace with state
-    state_subset = df[df["State_Name"] == "Andaman and Nicobar Islands"]
+    state_subset = df[df["State_Name"] == state]
     for year in year_list:
         year_subset = state_subset[state_subset["Crop_Year"] == year]
         if not year_subset.empty:
             crop_subset = year_subset[year_subset["Crop"] == crop]
-            produce = sum(crop_subset["Production"]) / sum(crop_subset["Area"])
-            dataset = dataset.append(
-                {'State': state, 'Year': year, 'Produce': produce, 'Temperature': 10, 'Rainfall': 10},
-                ignore_index=True)
-            # remove these breaks later
-            break
-    # remove these breaks later
-    break
+            if not crop_subset.empty:
+                produce = sum(crop_subset["Production"]) / sum(crop_subset["Area"])
 
-print("Final Dataset :: ")
-print(str(dataset))
+                rainfall_state_df = rainfall_df[rainfall_df["SUBDIVISION"].str.contains(state)]
+                rainfall_year_df = rainfall_state_df[rainfall_state_df["YEAR"] == year]
+
+                temp_year_df = temperature_df[temperature_df["Year"] == year]
+                temp_state_df = temp_year_df[state]
+
+                if not rainfall_year_df.empty and not temp_state_df.empty:
+                    dataset = dataset.append(
+                        {'State': state, 'Year': year, 'Produce': produce,
+                         'Temperature': temp_state_df.iloc[0],
+                         'Rainfall': rainfall_year_df["ANNUAL"].iloc[0]},
+                        ignore_index=True)
+
+dataset.to_csv(crop + ".csv", index=False)
