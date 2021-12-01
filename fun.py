ec4ea4a31a70455b41d0f36060d67221123db245
aakash ranglani <ranglaniaakash.git@gmail.com>
2021-12-01 01:07:21 +0000

    replaced builtin sum with pandas sum

--- fun.py
+++ fun.py
@@ -21,14 +21,23 @@ df['State_Name'] = df['State_Name'].str.strip()
 #     temperature_df.columns.drop(list(temperature_df.filter(regex='Administrative unit not ava*')))]
 
 for state in state_list:
-    # remove "Andaman and Nicobar Islands" and replace with state
+    state = "Tamil Nadu"
     state_subset = df[df["State_Name"] == state]
+    # district_list = state_subset["District_Name"].unique()
+    # print(len(district_list))
     for year in year_list:
         year_subset = state_subset[state_subset["Crop_Year"] == year]
         if not year_subset.empty:
             crop_subset = year_subset[year_subset["Crop"] == crop]
             if not crop_subset.empty:
-                produce = sum(crop_subset["Production"]) / sum(crop_subset["Area"])
+                total_production = crop_subset["Production"].sum()
+                total_area = crop_subset["Area"].sum()
+                produce = total_production / total_area
+
+                # Ignore
+                # total_production_sum = sum(crop_subset["Production"])
+                # total_area_sum = sum(crop_subset["Area"])
+                # produce_sum = total_production_sum / total_area_sum
 
                 rainfall_state_df = rainfall_df[rainfall_df["SUBDIVISION"].str.contains(state)]
                 rainfall_year_df = rainfall_state_df[rainfall_state_df["YEAR"] == year]
@@ -37,7 +46,22 @@ for state in state_list:
                 temp_year_df = temperature_df[temperature_df["Year"] == year]
                 temp_state_df = temp_year_df[state]
 
-                if not rainfall_year_df.empty and not temp_state_df.empty and not pd.isna(produce):
+                # we can remove these lines later, before code submission... they help in debugging a bit
+                # if not rainfall_year_df.empty and not temp_state_df.empty and (not pd.isna(produce)):
+                #     print(
+                #         "State : " + str(state) + ", Year : " + str(year) + ", Produce : " + str(
+                #             produce) + ", Temp : " + str(temp_state_df.iloc[0]) + ", Rainfall : " + str(
+                #             rainfall_annual_df.iloc[0]))
+                # else:
+                #     print("EMPTY!!!")
+                #     print("Sum of Produce :: " + str(sum(crop_subset["Production"])))
+                #     print("Sum of Area :: " + str(sum(crop_subset["Area"])))
+                #     print(
+                #         "State : " + str(state) + ", Year : " + str(year) + ", Produce : " + str(
+                #             produce) + ", Temp : " + str(temp_state_df) + ", Rainfall : " + str(
+                #             rainfall_annual_df))
+
+                if not rainfall_year_df.empty and not temp_state_df.empty and (not pd.isna(produce)):
                     dataset = dataset.append(
                         {'State': state, 'Year': year, 'Produce': produce,
                          'Temperature': temp_state_df.iloc[0],
