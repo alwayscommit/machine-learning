0844003c7c38e579610d8e12291e40709ead6f79
dee-vy <48962981+dee-vy@users.noreply.github.com>
2021-12-05 16:01:49 +0000

    Added tweaks
    Trying to work next:
    # X = np.column_stack((X1, X2))
    # Xpoly = PolynomialFeatures(5).fit_transform(X)
    # X_train, X_test, y_train, y_test = train_test_split(Xpoly, y, test_size=0.20)
    # X_train, y_train = np.meshgrid(X_train, y_train)

--- plot3d.py
+++ plot3d.py
@@ -4,10 +4,13 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import Lasso
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+from math import sqrt
 
-df = pd.read_csv("Banana.csv")
-label = "Banana Crop"
+df = pd.read_csv("Turmeric.csv")
+label = "Turmeric Crop"
 df = df.dropna()
 
 X_original = df[['Temperature', 'Rainfall']]
@@ -23,13 +26,24 @@ X2 = X_original.iloc[:, 1]
 # model.fit(X_train, y_train.values.ravel())
 # yPred = model.predict(X_test)
 
+
 # Polynomial features with RandomForestRegressor
+# 1. Three features
 # X3 = X1 * X2
 # X = np.column_stack((X1, X2, X3))
-# model = RandomForestRegressor()
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
-# model.fit(X_train, y_train.values.ravel())
-# yPred = model.predict(X_test)
+
+# 2. Square
+X1 = X1 * X1
+X2 = X2 * X2
+X = np.column_stack((X1, X2))  # r2 is 0.74 for Turmeric
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+# Model
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
+yPred = model.predict(X_test)
+
 
 # Original features with knn
 # X = np.column_stack((X1, X2))
@@ -53,7 +67,7 @@ X2 = X_original.iloc[:, 1]
 # model.fit(X_train, y_train)
 # yPred = model.predict(X_test)
 
-# Original features with Lasso (Still Bad)
+# Polynomial features with Lasso (Still Bad)
 # X3 = X1 * X2
 # X = np.column_stack((X1, X2, X3))
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
@@ -61,6 +75,7 @@ X2 = X_original.iloc[:, 1]
 # model.fit(X_train, y_train)
 # yPred = model.predict(X_test)
 
+# Plots
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.set_title(label)
@@ -73,3 +88,9 @@ predicted_val._facecolors2d = predicted_val._facecolor3d
 predicted_val._edgecolors2d = predicted_val._edgecolor3d
 plt.legend(loc="best")
 plt.show()
+
+# Scores
+print(model.score(X_test, y_test))
+print("Mean squared error: %.2f" % mean_squared_error(y_test, yPred))
+print("Root mean squared error: %.2f" % sqrt(mean_squared_error(y_test, yPred)))
+print("r2 square: %.2f" % r2_score(y_test, yPred))
