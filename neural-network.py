6082d32664f0e8a219f6ca571ab5f15b6717713e
aakash ranglani <ranglaniaakash.git@gmail.com>
2021-12-06 17:02:37 +0000

    svm , neural network

---
+++ neural-network.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import cross_val_score, train_test_split
+import tensorflow as tf
+from keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import mean_squared_error, r2_score
+from math import sqrt
+from sklearn.dummy import DummyRegressor
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv("crop_dataset/Turmeric.csv")
+
+X = df[['Temperature', 'Rainfall']]
+y = df['Produce']
+
+y = y.values.reshape(-1, 1)
+
+X_scaler = StandardScaler()
+y_scaler = StandardScaler()
+
+X = X_scaler.fit_transform(X)
+y = y_scaler.fit_transform(y)
+
+X1 = X[:, 0]
+X2 = X[:, 1]
+X3 = X1 * X2
+X = np.column_stack((X1, X2, X3))
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2)
+
+input_size = 3
+output_size = 1
+model = tf.keras.Sequential([tf.keras.layers.Dense(output_size)])
+model.add(Dense(128, activation="relu", input_dim=6))
+model.add(Dense(32, activation="relu"))
+model.add(Dense(8, activation="relu"))
+model.add(Dense(1, activation="linear"))
+model.compile(optimizer=Adam(lr=1e-3, decay=1e-3 / 200), loss='mean_squared_error')
+model.fit(Xtrain, ytrain, validation_data=(Xtest, ytest), epochs=10000, batch_size=500, verbose=2)
+
+yPred = model.predict(Xtest)
+
+print("Root mean squared error: %.2f" % sqrt(mean_squared_error(ytest, yPred)))
+print("r2 square: %.2f" % r2_score(ytest, yPred))
+
+dummy_regr = DummyRegressor(strategy="mean")
+dummy_regr.fit(Xtrain, ytrain)
+dummy_pred = dummy_regr.predict(Xtest)
+
+print("Dummy - Root mean squared error: %.2f" % sqrt(mean_squared_error(ytest, dummy_pred)))
+print("Dummy - r2 square: %.2f" % r2_score(ytest, dummy_pred))
+
